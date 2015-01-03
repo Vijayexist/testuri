7c2e8fb3c2e34e60232f910773e3a4ea08738f31
SatlawarV <vijayexist@gmail.com>
2015-01-04 00:37:00 +0530

    test

---
+++ HelloWorldExample/src/com/tutorials4u/helloworld/HelloWorld.java
@@ -0,0 +1,53 @@
+package com.tutorials4u.helloworld;
+
+
+/**
+ *
+ * @author info@tutorials4u.net
+ */
+
+public class HelloWorld {
+
+	private static final long serialVersionUID = 1L;
+
+	private String message;
+
+    private String userName;
+    
+    public HelloWorld() {
+    }
+
+    public String execute() {
+        setMessage("Hello " + getUserName());
+        return "SUCCESS";
+    }
+
+    /**
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+}
